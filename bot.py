63605c41ed17071bc23105ca5ab59d7c969e868d
jaylahsifuentes52-boop <jaylahsifuentes52@gmail.com>
2026-09-14 19:38:35 -0400

    Update TOKEN retrieval method for Discord bot

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-TOKEN = os.getenv("DISCORD_TOKEN MTU0ODc3ODcyMDQzOTk1OTcwMg.GlrK4R.YD4Wd-l2SplcfEQCAwE32u8gQXWIGGSO3OSd7w")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 if not TOKEN:
     raise RuntimeError("DISCORD_TOKEN is not set in Render.")
